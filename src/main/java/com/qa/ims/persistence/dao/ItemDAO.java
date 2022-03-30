a978635a87185eec6b730b2221fc9ca607c56629
Salim1704 <qhs9876@gmail.com>
2022-03-30 16:25:18 +0100

    IMS-3 IMS-5 IMS-8 IMS-7 #initial commit of new item classess

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,34 @@
+package com.qa.ims.persistence.dao;
+
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Item;
+
+public class ItemDAO {
+
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Item create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Item create(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Item update(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int delete(Long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
